ff95ccf300c431daea5482c583889df053ecc35c
zhao-gao-sheng <594zhaogaosheng@gmail.com>
2016-12-03 16:50:39 +0800

    upload src.dir
    上载src文件夹，用于装js文件

---
+++ src/Hello.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component{
+	render() {
+		return (
+			<div>
+				Hello, The React World!Could I coming?
+			</div>
+		);
+	}
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import React from './build/react';
+import ReactDOM from './build/react-dom';
+
+import App from './Hello.jsx'
